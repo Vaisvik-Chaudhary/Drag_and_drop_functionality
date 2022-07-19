afc0fcc73d5080b6c1623dbfd10f54b59280dc0b
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-07-19 17:05:10 +0530

    Functionality  updated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,14 @@
 class Post < ApplicationRecord
     has_many_attached :images
     validates :title, :body, presence: true
-
+   
+   
+    def images_as_thumbnails
+        images.map do |image|
+            image.variant(resize_to_limit: [150, 150]).processed
+        end
+    end
+    def picture_as_thumbnail(pic)
+        pic.variant(resize_to_limit: [200, 200]).processed 
+    end
 end
