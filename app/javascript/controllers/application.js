974fd0be739e8b17c1a819a7299335058f4578f0
Vaisvik-Chaudhary <vaisvik.chaudhary@bacancy.com>
2022-07-19 13:18:58 +0530

    Implemented drag and drop functionality

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,13 @@
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+import ActiveStorageDragAndDrop from 'active_storage_drag_and_drop'
+
+ActiveStorageDragAndDrop.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
